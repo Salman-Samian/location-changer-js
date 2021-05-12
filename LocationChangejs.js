73a668aeb9a293628d2c498d8a5bdf62a5e9f56a
Salman <salman.samian@gmail.com>
2021-05-12 19:30:35 +0430

    Add files via upload

---
+++ LocationChangejs.js
@@ -0,0 +1,23 @@
+const LocationChangerJs = {};
+
+
+LocationChangerJs.getCurrentURLPath = async (href) => {
+     try {
+          var strlUrl = href.split('/');
+          var strlUrlE = "/";
+          for (var i = 3; i < strlUrl.length; i++) {
+               if (strlUrl.length == (i + 1)) {
+                    strlUrlE += (strlUrl[i].split('#')[0]);
+               } else {
+                    strlUrlE += strlUrl[i] + "/";
+               }
+          }
+
+          return strlUrlE;
+     } catch (err) {
+          return err;
+     }
+};
+
+
+export default LocationChangerJs;
